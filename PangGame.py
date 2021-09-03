30151258d01327c455c1b0f8a2879cc6253179e6
Jinhyeok Jeon <aqwstn@naver.com>
2021-09-03 19:12:53 +0900

    Initial commit

---
+++ PangGame.py
@@ -0,0 +1,103 @@
+import os
+import pygame
+######################################################################
+# 초기화
+pygame.init()
+
+# 화면 크기 설정
+screen_width = 640  # 가로 크기
+screen_height = 480  # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+pygame.display.set_caption("Pang")  # 게임 이름
+clock = pygame.time.Clock()
+
+current_path = os.path.dirname(__file__)
+
+image_path = os.path.join(current_path, "images")
+background = pygame.image.load(os.path.join(image_path, "background.png"))
+
+stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+stage_size = stage.get_rect().size
+stage_height = stage_size[1]  # 스테이지의 높이
+
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height - stage_height
+
+# 캐릭터 이동 방향
+character_to_x = 0
+
+# 캐릭터 이동 속도
+character_speed = 5
+
+# 무기
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+# 무기는 한 번에 여러 발 발사 가능
+weapons = []
+
+# 무기 이동 속도
+weapon_speed = 10
+# 공 만들기
+"""
+ball_images = [
+    pygame.image.load(os.path.join(image_path, "balloon1.png")),
+    pygame.image.load(os.path.join(image_path, "balloon2.png")),
+    pygame.image.load(os.path.join(image_path, "balloon3.png")),
+    pygame.image.load(os.path.join(image_path, "balloon4.png"))
+]
+"""
+#####################################################################
+
+
+running = True
+while running:
+    dt = clock.tick(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE:  # 무기 발사
+                weapon_x_pos = character_x_pos + (character_width / 2) - (weapon_width / 2)
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 무기 위치 조정
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]  # 무기 위치를 위로
+
+    # 천장에 무기 닿으면 삭제
+    weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
+
+    screen.blit(background, (0, 0))
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
+    screen.blit(stage, (0, screen_height - stage_height))
+
+    screen.blit(character, (character_x_pos, character_y_pos))
+
+    pygame.display.update()
+
+pygame.quit()
